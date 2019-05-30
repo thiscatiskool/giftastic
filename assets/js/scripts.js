377b069e1521368a77c79be85ab62a1a7ac3422e
Winston Kyle Davis <thiscatiskool@Winstons-MacBook-Pro.local>
2019-05-30 18:32:57 -0400

    adding another step to the JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,23 +3,43 @@
 
 var topics= ["planes", "trains", "automobiles"]
 
+
 //write a function to make a button with the string names from the array; a for loop that appends each string
 function printBtn(){
-    $('#buttons-div').empty();
+
+    $("#buttonsdiv").empty();
 for( i=0; i<topics.length; i++){
     var btn= document.createElement("button");
     var t= document.createTextNode(topics[i]);
     btn.appendChild(t);
     document.body.appendChild(btn);
-    //buttonObject.val = topics[i]
-    }
-}
-window.onload = printBtn();
 //assign the buttons a value based on the string associated with it in the array
+    btn.value = topics[i];
+    
+    }
+    
+}
+//window.onload = printBtn("#buttonsdiv");
+
 
 //write a function to get the gifs from the giphy website using the api
+function getData(){
 
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + + "&api_key=YERQXnmfbNKMHk1LmAm76UTeH1TPXVQj&limit=10");
+xhr.done(function(response) { 
+    console.log("success got data", response);
+
+var jiff= response.data
+
+    for (i in jiff) {
+    $('.inner').append("<img src='"+jiff[i].images.original.url+"' style= 'height:200px; width:200px;'/>")
+        }
+    });
+}
 //write a function that assigns a variable a value when a topics button is clicked
+
+//var chosenTopic= $(btn).click()
 
 //write another funtction that takes the above variable of the gifName and places it into the search critera in the api url
 
@@ -37,6 +57,9 @@ window.onload = printBtn();
 
 //Next write a function to take the user inputs from the search bar
 
+//var input= $("#searchtext").val()
+
+
 //turn the user inputs into a string and place it into the array
 
 //Make sure that these new array strings create a button same as the others
@@ -44,36 +67,5 @@ window.onload = printBtn();
 //clear out the search bar so that a new search query can be entered
 
 //Write a function that if the same topics button is clicked additional images will be displayed instead of resetting the DOM
-
-
-
-
-
-
-function getData(){
-
-    var input= $("#searchtext").val()
-
-//javascript, jQuery
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=cat&api_key=YERQXnmfbNKMHk1LmAm76UTeH1TPXVQj&limit=10");
-xhr.done(function(response) { 
-    console.log("success got data", response);
-
-var jiff= response.data
-//var gifs= ["cars, fish, anna+kendrick"]
-
-for (i in jiff) {
-//for(var i =0; i<arr.length; i++) {
-    $('.inner').append("<img src='"+jiff[i].images.original.url+"' style= 'height:200px; width:200px;'/>")
-}
-});
-
-
-
-}
-
-
-
-
 
 
